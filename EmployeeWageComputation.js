e464c3695c9da3faedc9a24a245587ee2b0bc0b1
pavanyalala <pavanyalala4508@gmail.com>
2021-09-10 23:40:08 +0530

    [pavan]:calculated wage for one month

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -4,8 +4,8 @@ const IS_FULL_TIME =2;
 const PART_TIME_HOURS =4;
 const FULL_TIME_HOURS=8;
 const WAGE_PER_HOURS=20;
+const WORKING_DAYS=20;
 let empHrs=0;
-let empCheck=Math.floor(Math.random()*10)%3;
 function getWorkingHours(empCheck)
 {
     switch(empCheck)
@@ -18,6 +18,10 @@ function getWorkingHours(empCheck)
             return 0;
     }
 }
-empHrs=getWorkingHours(empCheck);
+for(let day=0;day<WORKING_DAYS;day++)
+{
+    let empCheck = Math.floor(Math.random()*10)%3;
+    empHrs += getWorkingHours(empCheck);
+}
 let empWage= empHrs*WAGE_PER_HOURS;
 console.log("Employee Wage: "+ empWage);
